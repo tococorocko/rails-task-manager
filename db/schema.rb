206d9f58872a865926d29334604a1b784a3b62ee
Lucca Kaiser <luccakaiser@gmail.com>
2018-11-13 16:15:37 +0100

    create task manager

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_11_13_104317) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
